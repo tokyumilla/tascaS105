d0e81b4b583d8204d72cd485727ae91856ebcf19
tokyumilla <tokyumilla@gmail.com>
2022-11-21 18:28:05 +0100

    Ejercicio 2 terminado

--- src/n1exercici3/Main.java
+++ src/n1exercici3/Main.java
@@ -1,10 +1,9 @@
-package n1exercici2;
+package n1exercici3;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.Files;
 import java.text.Collator;
-import java.util.Arrays;
 import java.util.TreeSet;
 
 public class Main {
